b8e8eb7ab6443afe5f472b447a49eaa27711cc35
Sowu20 <sowukelly67@gmail.com>
2026-05-18 17:32:17 +0000

    feat: Configuration Resend pour l'envoie par email

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,10 +1,35 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function POST(request) {
-    const body = await request.json();
+    try {
+        const body = await request.json();
+        const { nom, email, objet } = body;
 
-    console.log("Données reçues :", body);
+        const response = await resend.emails.send({
+            from: "CYBELL-SANTE <onboarding@resend.dev>",
+            to: ["sowukelly67@gmail.com"],
+            subject: "Nouveau message",
+            html: `
+                <h2>Nouveau contact</h2>
+                <p><strong>Nom :</strong> ${nom}</p>
+                <p><strong>Email :</strong> ${email}</p>
+                <p><strong>Message :</strong> ${objet}</p>
+            `
+        });
 
-    return Response.json({
-        message: "Message reçu avec succès",
-        data: body,
-    });
+        return Response.json({
+            success: true,
+            response,
+        });
+    } catch (error) {
+        return Response.json(
+            {
+                success: false,
+                error,
+            },
+            { status: 500 }
+        );
+    }
 }
