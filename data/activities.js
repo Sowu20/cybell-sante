b696eaceb1315245f086be2a8888681ae3acbaee
Sowu20 <sowukelly67@gmail.com>
2026-05-20 11:08:59 +0000

    feat: Modification de la description des activités

--- data/activities.js
+++ data/activities.js
@@ -1,32 +1,32 @@
 export const activities = [
   {
-    title: "Jeu de société",
-    description: "Support ludique de sensibilisation à la santé.",
+    title: "Jeu de société CYBELL-SANTE",
+    description: "Un coffret ludique comprenant 7 mains de cartes soigneusement conçues. Chaque main représente un thème de santé majeure, permettant d'apprendre les bons réflexes tout en s'amusant.",
     icon: "casino",
   },
   {
-    title: "Clubs scolaires",
-    description: "Création de clubs éducatifs dans les établissements.",
+    title: "Clubs CYBELL-SANTE",
+    description: "Création de clubs d'utilisateurs dynamiques au sein des écoles et collèges. Nous favorisons une émmulation saine pour encourager l'adoption de comportements responsables.",
     icon: "school",
   },
   {
     title: "Émissions radio",
-    description: "Sensibilisation via les radios de proximité.",
+    description: "Transposition du concept de jeu sur les ondes locales. Nous atteignons les populations directement dans leur quotidien pour diffuser massivement de prévention",
     icon: "radio",
   },
   {
     title: "Émissions télévision",
-    description: "Programmes interactifs de vulgarisation.",
+    description: "Production et diffusion de programmes télévisés interactifs pour sensibiliser les jeunes et les familles aux enjeux de santé publique à travers des contenus éducatifs accessibles.",
     icon: "tv",
   },
   { 
     title: "Jeu en ligne",
-    description: "Version numérique accessible sur internet.",
+    description: "Plateforme numérique interactive permettant aux utilisateurs d’apprendre les bonnes pratiques de santé à travers des quiz, défis et jeux pédagogiques accessibles sur internet.",
     icon: "sports_esports",
   },
   {
     title: "CD-ROM éducatif",
-    description: "Support pour apprendre hors connexion.",
+    description: "Support multimédia conçu pour l’apprentissage hors connexion, offrant des contenus éducatifs interactifs sur la prévention et la sensibilisation aux principales questions de santé.",
     icon: "album",
   },
 ];
