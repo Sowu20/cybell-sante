7668873dbe02120647a01ee584afa77bbed3eda3
Sowu20 <sowukelly67@gmail.com>
2026-05-22 10:02:54 +0000

    feat: Modification des données

--- data/services.js
+++ data/services.js
@@ -1,14 +1,20 @@
 export const services = [
   {
     title: "Jeu éducatif",
-    description: "Un support ludique pour sensibiliser les jeunes à la santé.",
+    description: "Un support ludique et complet pour sensibiliser les jeunes aux questions de santé majeures.",
+    icon: "casino",
+    borderColor: "border-primary"
   },
   {
     title: "Clubs scolaires",
-    description: "Des clubs dans les établissements pour apprendre ensemble.",
+    description: "Des clubs dynamiques au sein des établissements pour apprendre, partager et s'émuler ensemble.",
+    icon: "school",
+    borderColor: "border-secondary"
   },
   {
     title: "Plateforme numérique",
-    description: "Une version digitale accessible sur internet.",
-  },
+    description: "Une version digitale et interactive accessible partout pour toucher les générations connectées.",
+    icon: "sports_esports",
+    borderColor: "border-tertiary"
+  }
 ];
